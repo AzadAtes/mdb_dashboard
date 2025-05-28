1606ca4189ead66a7e1016aad27ed026f07fa2cf
Marius Färfers <M.Faerfers@verband.creditreform.de>
2025-05-28 10:30:47 +0200

    Erste Schritte

---
+++ src/components/MainContent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="row">
+    <div>
+      <picture>
+        <img class="picture"
+            src="../../images/Rorlino.jpg" alt="Italian Trulli">
+      </picture>
+    </div>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.row {
+  background-color: #202020;
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+}
+.picture {
+  height: 500px;
+}
+</style>
